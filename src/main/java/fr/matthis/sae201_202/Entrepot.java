b72eecea2aaa33f4bbaa6fcb1b3773923f58d14d
jubrg <julien.bourgeois4@icloud.com>
2024-04-17 10:06:48 +0200

    Julien : Remplissage classe Entrepot

--- src/main/java/fr/matthis/sae201_202/Entrepot.java
+++ src/main/java/fr/matthis/sae201_202/Entrepot.java
@@ -3,17 +3,27 @@ package fr.matthis.sae201_202;
 public class Entrepot extends Sector {
 
     public Entrepot() {
+        this.id = 0;
+        this.type = null;
+        this.position = new Coordonnee(0,0);
+        this.capacity = 0;
+        this.maxCapacity = 999999999;
+    }
+    public Entrepot(int id, Ore type, int x, int y) {
+        super();
+        this.id = id;
+        this.type = type;
+        this.position = new Coordonnee(x,y);
     }
 
     private int id;
+    private Ore type;
 
     public String toString() {
-        // TODO implement here
-        return "";
+        return "E" + this.id + this.position.getX() + this.position.getY() + this.type + this.capacity;
     }
 
-    public int deposer(int quantite) {
-        // TODO implement here
-        return 0;
+    public void deposer(int qte) {
+        this.capacity += qte;
     }
 }
