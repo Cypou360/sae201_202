02a966edf1f7df9c57b3e36e0c45b8c2d859f819
Cypou360 <cyprienroyant@gmail.com>
2024-04-17 09:27:10 +0200

    Cyprien : class Mine constructeur et toString

--- src/main/java/fr/matthis/sae201_202/Mine.java
+++ src/main/java/fr/matthis/sae201_202/Mine.java
@@ -1,10 +1,14 @@
 package fr.matthis.sae201_202;
 
+import java.util.Random;
+
 public class Mine extends Sector {
 
     public Mine() {
         super();
-
+        Random r = new Random();
+        this.maxCapacity = r.nextInt(50,100);
+        this.capacity = this.maxCapacity;
     }
 
     private Ore minerai;
@@ -12,7 +16,7 @@ public class Mine extends Sector {
     private int id;
 
     public String toString() {
-        return "out";
+        return "/ " + minerai + " " + id + " /";
     }
     public Ore getMinerai() {
         return minerai;
