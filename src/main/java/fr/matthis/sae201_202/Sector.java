31d077437628a64b2da87b841ada024807115223
Cypou360 <cyprienroyant@gmail.com>
2024-04-17 09:25:24 +0200

    Cyprien : correction faute Sector

--- src/main/java/fr/matthis/sae201_202/Sector.java
+++ src/main/java/fr/matthis/sae201_202/Sector.java
@@ -3,20 +3,20 @@ package fr.matthis.sae201_202;
 public abstract class Sector {
 
     public Sector() {
-        this.maxCapicty = 0;
+        this.maxCapacity = 0;
         this.capacity = 0;
         this.disponible = true;
         this.position = new Coordonnee(0, 0);
     }
 
     public Sector(int x, int y) {
-        this.maxCapicty = 0;
+        this.maxCapacity = 0;
         this.capacity = 0;
         this.disponible = true;
         this.position = new Coordonnee(x, y);
     }
 
-    protected int maxCapicty;
+    protected int maxCapacity;
 
     protected int capacity;
 
