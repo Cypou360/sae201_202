f273b32abc6207cf5210313ceff01f0e4a223282
matthis lustremant <Matthis.Lustremant@uphf.Fr>
2024-04-18 11:31:20 +0200

    Matthis: Changement de game

--- src/main/java/fr/matthis/sae201_202/Game.java
+++ src/main/java/fr/matthis/sae201_202/Game.java
@@ -10,7 +10,7 @@ public class Game {
         boolean bool = true;
         int nbtour = 1;
         System.out.println(grille);
-        while (bool){
+        while (bool) {
             System.out.println("tour : " + nbtour + "\n");
 
             System.out.println("quel id de robots voulez vous déplacez : ");
@@ -20,40 +20,36 @@ public class Game {
             System.out.println("Donnez une direction: Droite(1), Gauche(2), Haut(3), Bas(4) :");
             Scanner nb = new Scanner(System.in);
             int nombre = nb.nextInt();
-
-            if (nombre == 1){
-                if(grille.getRobot(idRobots).goTo("E",grille)){
-                    System.out.println("Déplacement Impossible");
+            int tmp = grille.getNbRobot();
+            if (tmp != 0) {
+                tmp--;
+                if (nombre == 1) {
+                    if (grille.getRobot(idRobots).goTo("E", grille)) {
+                        System.out.println("Déplacement Impossible");
+                    } else {
+                        nbtour += 1;
+                    }
+                } else if (nombre == 2) {
+                    if (grille.getRobot(idRobots).goTo("O", grille)) {
+                        System.out.println("Déplacement Impossible");
+                    } else {
+                        nbtour += 1;
+                    }
+                } else if (nombre == 3) {
+                    if (!grille.getRobot(idRobots).goTo("N", grille)) {
+                        System.out.println("Déplacement Impossible");
+                    } else {
+                        nbtour += 1;
+                    }
+                } else if (nombre == 4) {
+                    if (grille.getRobot(idRobots).goTo("S", grille)) {
+                        System.out.println("Déplacement Impossible");
+                    } else {
+                        nbtour += 1;
+                    }
                 }
-                else{
-                    nbtour += 1;
-                }
+                System.out.println(grille);
             }
-            else if (nombre == 2) {
-               if(grille.getRobot(idRobots).goTo("O",grille)){
-                   System.out.println("Déplacement Impossible");
-               }
-               else{
-                   nbtour += 1;
-               }
-            }
-            else if (nombre == 3) {
-                if(!grille.getRobot(idRobots).goTo("N",grille)){
-                    System.out.println("Déplacement Impossible");
-                }
-                else{
-                    nbtour += 1;
-                }
-            }
-            else if (nombre == 4) {
-                if(grille.getRobot(idRobots).goTo("S",grille)){
-                    System.out.println("Déplacement Impossible");
-                }
-                else{
-                    nbtour += 1;
-                }
-            }
-            System.out.println(grille);
         }
     }
 }
