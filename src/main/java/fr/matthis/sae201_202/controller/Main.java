56563d55d32398fef0c454a40b000822200de393
Cypou360 <cyprienroyant@gmail.com>
2024-04-22 17:04:56 +0200

    Cyprien : creation lancemant graphique

---
+++ src/main/java/fr/matthis/sae201_202/controller/Main.java
@@ -0,0 +1,15 @@
+package fr.matthis.sae201_202.controller;
+
+import javafx.application.Application;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(javafx.stage.Stage primaryStage) {
+        primaryStage.setTitle("Robot");
+        primaryStage.show();
+    }
+}
