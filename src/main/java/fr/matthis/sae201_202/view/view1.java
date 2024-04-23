43f8750e6728f16b97884cdb303f352feb2044e1
matthis lustremant <Matthis.Lustremant@uphf.Fr>
2024-04-23 10:07:47 +0200

    Matthis: ajout pour graphique JavaFX

--- src/main/java/fr/matthis/sae201_202/view/view1.java
+++ src/main/java/fr/matthis/sae201_202/view/view1.java
@@ -1,4 +1,22 @@
 package fr.matthis.sae201_202.view;
 
-public class view1 {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class view1 extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Scene scene = new Scene(new Mainview(20.0));
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
