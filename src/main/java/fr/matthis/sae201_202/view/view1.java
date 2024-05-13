e4ebfb744a312a3a8ad0e840a6bd45b46d4db5a2
Cypou360 <cyprienroyant@gmail.com>
2024-05-13 11:38:35 +0200

    Cyprien : génération grille

--- src/main/java/fr/matthis/sae201_202/view/view1.java
+++ src/main/java/fr/matthis/sae201_202/view/view1.java
@@ -1,6 +1,9 @@
 package fr.matthis.sae201_202.view;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class view1 extends Application {
@@ -11,6 +14,25 @@ public class view1 extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Robot");
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 800);
+        primaryStage.setScene(scene);
+        gridgen(root);
+
         primaryStage.show();
+    }
+
+    public void gridgen(Group g) {
+        int prevX = 50;
+        int prevY = 50;
+        for (int i = 0; i <= 10; i++) {
+            Line l = new Line(prevX, 50, prevX, 50+50*10);
+            prevX += 50;
+            g.getChildren().add(l);
+
+            Line l2 = new Line(50, prevY, 50+50*10, prevY);
+            prevY += 50;
+            g.getChildren().add(l2);
+        }
     }
 }
