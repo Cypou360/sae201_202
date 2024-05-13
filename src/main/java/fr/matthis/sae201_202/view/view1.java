629dbdf62d7de5a9503e937aa12ae931498b028a
Cypou360 <cyprienroyant@gmail.com>
2024-05-13 12:29:56 +0200

    Cyprien : coloration cellule

--- src/main/java/fr/matthis/sae201_202/view/view1.java
+++ src/main/java/fr/matthis/sae201_202/view/view1.java
@@ -4,9 +4,14 @@ import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import fr.matthis.sae201_202.model.*;
 
 public class view1 extends Application {
     public static void main(String[] args) {
@@ -16,6 +21,9 @@ public class view1 extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+        Grille grid = new Grille();
+        grid.initialisation();
+
         Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
         double width = screenSize.getWidth();
         double height = screenSize.getHeight();
@@ -24,16 +32,55 @@ public class view1 extends Application {
         Group root = new Group();
         Scene scene = new Scene(root, width, height);
         primaryStage.setScene(scene);
-        gridgen(root, (int) scene.getHeight());
+        primaryStage.setResizable(false);
+        gridgen(root, (int) scene.getHeight(), grid);
 
         primaryStage.show();
     }
 
-    public void gridgen(Group g, int height) {
+    public void gridgen(Group g, int height, Grille grille) {
         int cellsize = (height-100)/10;
         int prevX = 50;
         int prevY = 50;
 
+        // dessin des Secteurs
+        Sector[][] grid = grille.getGrille();
+        for (Sector[] s : grid) {
+            for (Sector ss : s) {
+                if (ss instanceof Vide) {
+                    Vide v = ((Vide) ss);
+                    Coordonnee pos = v.getPosition();
+                    Rectangle r = new Rectangle(50 + pos.getX()*cellsize, 50 + pos.getY()*cellsize, cellsize, cellsize);
+                    r.setFill(Color.GREEN);
+                    g.getChildren().add(r);
+
+                } else if (ss instanceof Entrepot) {
+                    Entrepot e = ((Entrepot) ss);
+                    Coordonnee pos = e.getPosition();
+                    Rectangle r = new Rectangle(50 + pos.getX()*cellsize, 50 + pos.getY()*cellsize, cellsize, cellsize);
+                    r.setFill(Color.BROWN);
+                    String type = e.getType().toString();
+                    Text t = new Text(50 + pos.getX()*cellsize + 10, 50 + pos.getY()*cellsize + 10, type);
+                    g.getChildren().add(r);
+                    g.getChildren().add(t);
+
+                } else if (ss instanceof Mine) {
+                    Mine m = ((Mine) ss);
+                    Coordonnee pos = m.getPosition();
+                    Rectangle r = new Rectangle(50 + pos.getX()*cellsize, 50 + pos.getY()*cellsize, cellsize, cellsize);
+                    r.setFill(Color.GRAY);
+                    g.getChildren().add(r);
+
+                } else if (ss instanceof Lac) {
+                    Lac l = ((Lac) ss);
+                    Coordonnee pos = l.getPosition();
+                    Rectangle r = new Rectangle(50 + pos.getX()*cellsize, 50 + pos.getY()*cellsize, cellsize, cellsize);
+                    r.setFill(Color.BLUE);
+                    g.getChildren().add(r);
+                }
+            }
+        }
+        // dessin de la grille
         for (int i = 0; i <= 10; i++) {
             Line l = new Line(prevX, 50, prevX, cellsize*10+50);
             prevX += cellsize;
