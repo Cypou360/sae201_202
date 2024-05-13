899e114d119534a80d238e6139e897ece6e5639d
Lucien <lulunewerkowitsch@gmail.com>
2024-05-13 10:13:57 +0200

    Lucien : Test de créer une fenêtre .

--- src/main/java/fr/matthis/sae201_202/view/view1.java
+++ src/main/java/fr/matthis/sae201_202/view/view1.java
@@ -1,22 +1,20 @@
 package fr.matthis.sae201_202.view;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class view1 extends Application {
-
     @Override
-    public void start(Stage stage) throws IOException {
-        Scene scene = new Scene(new Mainview(20.0));
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage stage) {
+        javafx.scene.text.Text text = new Text(10, 40, "Hello bande de FDP");
+        Group group1 = new Group(text);
+        stage.setTitle("Robot Mineur");
+        Scene scene = new Scene(group1);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
