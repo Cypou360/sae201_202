87844600e1f5ef4d16aa8d1627ddb8a20305863e
Cypou360 <cyprienroyant@gmail.com>
2024-05-13 09:53:40 +0200

    Cyprien : MovementException

---
+++ src/main/java/fr/matthis/sae201_202/model/MovementException.java
@@ -0,0 +1,10 @@
+package fr.matthis.sae201_202.model;
+
+public class MovementException extends Exception{
+    public MovementException(String message) {
+        super(message);
+    }
+    public MovementException() {
+        super();
+    }
+}
