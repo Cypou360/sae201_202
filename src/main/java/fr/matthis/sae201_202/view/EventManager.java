653871d2eb19abe1066a93ca49ba9fa58a210a63
Cypou360 <cyprienroyant@gmail.com>
2024-05-16 15:54:57 +0200

    Cyprien : fix event manager

--- src/main/java/fr/matthis/sae201_202/view/EventManager.java
+++ src/main/java/fr/matthis/sae201_202/view/EventManager.java
@@ -1,24 +1,16 @@
 package fr.matthis.sae201_202.view;
 
 import fr.matthis.sae201_202.model.Sector;
-import fr.matthis.sae201_202.model.*;
-import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 
-public class eventManager implements EventHandler  {
+public class EventManager implements EventHandler  {
 
     private Main p;
 
-    public eventManager(Main p) {
+    public EventManager(Main p) {
         this.p = p;
     }
 
@@ -34,19 +26,6 @@ public class eventManager implements EventHandler  {
                 new infoCellule(s);
             }
         }
-
-    }
-}
-        }
-    }
-
-        stage.setScene(sc);
-        stage.setResizable(false);
-        stage.show();
-
-        root.getChildren().removeAll();
-
-
 
     }
 }
