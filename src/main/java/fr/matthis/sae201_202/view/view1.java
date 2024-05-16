eaf70eb3d397450a8891d4135638ad3303044aec
Lucien <lulunewerkowitsch@gmail.com>
2024-05-16 11:04:46 +0200

    Lucien : ajout du sous groupe des directions

--- src/main/java/fr/matthis/sae201_202/view/view1.java
+++ src/main/java/fr/matthis/sae201_202/view/view1.java
@@ -243,56 +243,61 @@ public class view1 extends Application {
         Group action = new Group();
         Button b = new Button("Extraire");
         Button b2 = new Button("Déposer");
+        b.setStyle("-fx-font: 15px \"None\";");
+        b.setPrefSize(90, 30);
+        b2.setStyle("-fx-font: 15px \"None\";");
+        b2.setPrefSize(90, 30);
         Label text = new Label("Selectionnez une action :");
         text.setFont(new Font(20));
         action.setTranslateX(1160);
         action.setTranslateY(60);
+        b.setTranslateY(60);
+        b2.setTranslateY(60);
+        b2.setTranslateX(150);
 
         action.getChildren().addAll(b,b2,text);
 
 
+        Group direction = new Group();
+        Label text2 = new Label("Selectionnez une direction :");
+        text2.setFont(new Font(20));
+        text2.setTranslateX(860);
+        text2.setTranslateY(210);
 
-        /*Button d1 = new Button("Nord");
-        d1.setTranslateX(860);
-        d1.setTranslateY(260);
+
+        Button d1 = new Button("Nord");
         d1.setPrefSize(90, 30);
         d1.setFont(new Font(15));
+        d1.setTranslateX(860);
+        d1.setTranslateY(250);
 
         Button d2 = new Button("Sud");
-        d2.setTranslateX(980);
-        d2.setTranslateY(260);
         d2.setPrefSize(90, 30);
         d2.setFont(new Font(15));
+        d2.setTranslateX(960);
+        d2.setTranslateY(250);
 
         Button d3 = new Button("Est");
-        d3.setTranslateX(1100);
-        d3.setTranslateY(260);
         d3.setPrefSize(90, 30);
         d3.setFont(new Font(15));
+        d3.setTranslateX(1060);
+        d3.setTranslateY(250);
 
         Button d4 = new Button("Ouest");
-        d4.setTranslateX(1220);
-        d4.setTranslateY(260);
         d4.setPrefSize(90, 30);
         d4.setFont(new Font(15));
+        d4.setTranslateX(1160);
+        d4.setTranslateY(250);
+        direction.getChildren().addAll(text2,d1,d2,d3,d4);
 
 
-
-
-
-
-
-        Text t3 = new Text(860, 230, "Selectionnez une direction :");
-        t3.setFont(new Font(20));
-
-        Text t4 = new Text(860, 360, "Récapitulatif :");
-        t4.setFont(new Font(20));*/
 
         g.getChildren().add(sidebar);
-        sidebar.getChildren().addAll(rectangle);
-        sidebar.getChildren().addAll(robot);
-        sidebar.getChildren().addAll(line);
-        sidebar.getChildren().addAll(action);
+        sidebar.getChildren().add(rectangle);
+        sidebar.getChildren().add(robot);
+        sidebar.getChildren().add(line);
+        sidebar.getChildren().add(action);
+        sidebar.getChildren().add(direction);
     }
 
 
