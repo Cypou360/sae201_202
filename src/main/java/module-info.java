4e6ce24641eb8806f0c7f776cfaede985c8a89d3
jubrg <julien.bourgeois4@icloud.com>
2024-05-24 09:33:19 +0200

    Julien : 2e Essai de correction du Junit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,10 @@
 module fr.matthis.sae201_202 {
-    requires javafx.controls;
-    requires org.junit.jupiter.api;
-    requires junit;
+        requires javafx.controls;
+        requires org.junit.jupiter.api;
+        requires junit;
+        requires org.junit.platform.commons;
 
-
-    exports fr.matthis.sae201_202.model;
-    exports fr.matthis.sae201_202.controller;
-    exports fr.matthis.sae201_202.view;
-
-}
+        exports fr.matthis.sae201_202.model;
+        exports fr.matthis.sae201_202.controller;
+        exports fr.matthis.sae201_202.view;
+        }
