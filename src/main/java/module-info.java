692e1b7e0cb99aa766694dc94306d2e86f4e9ce4
Lucien <lulunewerkowitsch@gmail.com>
2024-05-24 08:42:11 +0200

    Lucien : Création du dossier des livrables et ajout des livrable dedans et modification des noms des fichiers des diagramme de classe.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module fr.matthis.sae201_202 {
     requires org.junit.jupiter.api;
     requires junit;
 
+
     exports fr.matthis.sae201_202.model;
     exports fr.matthis.sae201_202.controller;
     exports fr.matthis.sae201_202.view;
+
 }
