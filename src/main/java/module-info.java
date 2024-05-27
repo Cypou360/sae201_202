feef2387cefb7a7213d9e390f64bda364b256f4c
Cypou360 <cyprienroyant@gmail.com>
2024-05-27 09:57:47 +0200

    Cyprien : update module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,13 @@ module fr.matthis.sae201_202 {
     requires javafx.controls;
     requires org.junit.jupiter.api;
     requires junit;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
 
     exports fr.matthis.sae201_202.model;
     exports fr.matthis.sae201_202.controller;
     exports fr.matthis.sae201_202.view;
+    opens fr.matthis.sae201_202;
 }
